f446180f8c87d72fa0c3c12e8625f33eac78af01
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2026-07-18 12:44:10 +0530

    first commit

---
+++ lambda/get/index.mjs
@@ -0,0 +1,46 @@
+
+import { DynamoDBDocumentClient, GetCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.tableName || "CoffeShop";
+
+const createResponse = (statusCode, body) => {
+    const responseBody = JSON.stringify(body);
+    return {
+        statusCode,
+        headers: { "Content-Type": "application/json" },
+        body: responseBody,
+    };
+};
+
+export const getCoffee = async (event) => {
+    const { pathParameters } = event;
+    const { id } = pathParameters || {};
+
+    try {
+        let command;
+        if (id) {
+            command = new GetCommand({
+                TableName: tableName,
+                Key: {
+                    "coffeeId": "c123",
+                },
+            });
+        }
+        else {
+            command = new ScanCommand({
+                TableName: tableName,
+            });
+        }
+        const response = await docClient.send(command);
+        return createResponse(200, response);
+    }
+    catch (err) {
+        console.error("Error fetching data from DynamoDB:", err);
+        return createResponse(500, { error: err.message });
+    }
+
+}
